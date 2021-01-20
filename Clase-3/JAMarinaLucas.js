c357609bb9135852fb9278826afa6114d9b5278a
Lucas Marina <lucasmarina26@gmail.com>
2021-01-20 09:13:46 -0300

    Desafio Entregable 3

--- Clase-3/JAMarinaLucas.js
+++ Clase-3/JAMarinaLucas.js
@@ -2,50 +2,29 @@ const finalizar = (cantidadPalabras) => {
     console.log(`Proceso completo, y hay ${cantidadPalabras} palabras`)
 }
 var id;
-let recorrerTexto = (string,callback,tiempoOpcional) => {
+
+let recorrerTexto = (string,tiempoOpcional = 1000, callback) => {
     let cantidad_de_palabras = 0;
     string = string.split(' ');
+
     id = setInterval(() => {
-        if (!(cantidad_de_palabras < string.length)) {
-            callback(cantidad_de_palabras);
-            clearInterval(id)
-        }else{
+        if (cantidad_de_palabras < string.length) {
             console.log(string[cantidad_de_palabras++])
+        } else {
+            clearInterval(id)
+            callback(string.length);
         }
-    }, (typeof(tiempoOpcional)==='undefined')?1000:tiempoOpcional)
+    }, tiempoOpcional, string)
+
 }
 
-// let recorrer = () => {
-//     // recorrerTexto('Hola Tutor soy tu alumno Lucas Marina',finalizar,() => {
-//         // recorrerTexto('Buenos Dias soy Lucas',finalizar,5000,() => {
-//         //     recorrerTexto('Buenas Tardes',finalizar);
-//         // });
-//         recorrerTexto('Hola Mundo',finalizar,() => {
-//             recorrerTexto('Chau',finalizar,2000)
-//         })
-//     // });
-// }
-// const promesa = recorrerTexto('Hola Mundo',finalizar)
-// console.log(promesa,'hola');
-
-
-function hacerAlgo() {
-    return new Promise( (resolve,reject) => {
-        console.log('Arrancan las funciones')
-        // recorrerTexto('Hola Mundo',finalizar)
-    } )
-}
-
-hacerAlgo()
-.then(
-    recorrerTexto('Hola Mundo',finalizar)
-    // console.log('1')
-)
-.then(
-    console.log('2')
-//     recorrerTexto('Buenos Dias soy Lucas',finalizar,5000)
-)
-.then(
-    console.log('3')
-//     recorrerTexto('Hola Tutor soy tu alumno Lucas Marina',finalizar)
-)
+recorrerTexto('Hola Mundo', 500, (count) => {
+    let totalPalabras = count;
+    recorrerTexto('2Hola 2Mundo', 500, (count) => {
+        totalPalabras += count;
+        recorrerTexto('3Hola 3Mundo', 500, (count) => {
+            totalPalabras += count;
+            finalizar(totalPalabras);
+        })
+    })
+})
