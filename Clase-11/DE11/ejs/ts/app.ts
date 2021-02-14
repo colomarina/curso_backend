b28be7df88f4bbb1da4ac86f7765dba287f76828
lucas-colito <lucasmarina26@gmail.com>
2021-02-14 17:36:43 -0300

    Desafio Entregable 10

---
+++ Clase-11/DE11/ejs/ts/app.ts
@@ -0,0 +1,36 @@
+import express from "express";
+import router from "../routes/routes";
+import handlebars from "express-handlebars";
+const path = require('path')
+
+
+// RUTAS DE LAS PLANTILLAS 
+// /api/agregar_producto  ---> agregar productos
+// /api/productos_vista  ---> lista los productos (la ruta que dice la consigna (/productos/vista) me da errores , por que al poner /productos/vista , la palabra vista me la toma como parametro del get /productos/:id y eso me interfiere , por eso la modifique!)
+
+
+const app = express();
+app.use(express.json())
+app.use(express.urlencoded({extended:true}));
+// Configuracion de Handlebars
+app.engine('hbs',
+    handlebars({
+        extname: '.hbs',
+        defaultLayout: 'index.hbs',
+        layoutsDir: __dirname + '/views/layouts/',
+        partialsDir: __dirname +  '/views/partials/',
+    })
+)
+// Establecemos el motor de plantilla que vamos a utilizar!
+app.set('view engine', 'hbs');
+// Establecemos el directorio donde se encuentran los archivos de plantillas
+app.set('views',path.join(__dirname, '/views'))
+// El agregar lo sincronice con esta plantilla , asi utilizaba ambos casos
+app.use('/api/agregar_producto',express.static('public'))
+app.use('/api', router)
+
+const port = 8080;
+const server = app.listen(port,()=>{
+    console.log(`El servidor se encuentra en el puerto: 8080`)
+})
+server.on("error", error => console.log(`Error en el servidor, ${error}`))
