1f073016270083895fb7a80d07ab37373295c5cb
lucas-colito <lucasmarina26@gmail.com>
2021-04-19 20:28:37 -0300

    Modifique la secret del session

--- Clase-24/DE24/ts/server/server.ts
+++ Clase-24/DE24/ts/server/server.ts
@@ -26,7 +26,7 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(cookieParser())
 app.use(session({
-  secret: 'colito753951',
+  secret: 'kira753951',
   resave: false,
   saveUninitialized: true,
   rolling:true,
